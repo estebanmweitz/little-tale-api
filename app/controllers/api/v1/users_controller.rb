c8293c4dc22395dbfd99a6e640cc894e9296ed25
rafaeldault <rafaeldault@gmail.com>
2020-04-22 13:52:21 -0500

    basic controller actions for User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,5 +10,28 @@ class Api::V1::UsersController < ApplicationController
         render json: users, status: 200
     end
 
-    
+    def create
+        user = User.create(user_params)
+        render json: users, status: 200
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: users, status: 200
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.delete
+
+        render json: {userId: user.id}
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
