cba2149efe4be32af67263f68235b5d7eddd5486
rafaeldault <rafaeldault@gmail.com>
2020-04-22 14:23:49 -0500

    adds basic comment controller actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        comments = Comment.all
+        render json: comments, status: 200
+    end
+
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment, status: 200
+    end
+
+    def create
+        comment = Comment.create(comment_params)
+        render json: comment, status: 200
+    end
+
+    def update
+        comment = Comment.find(params[:id])
+        comment.update
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text, :story_part, :user_id, :post_id)
+    end
+    
 end
