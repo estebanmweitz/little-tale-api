d7741930f2807c6392f52880b0c1e58b75a166f2
rafaeldault <rafaeldault@gmail.com>
2020-04-22 13:52:10 -0500

    basic controller actions for User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
     def index
-        @user = User.all
+        user = User.all
+        render json: users, status: 200
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: users, status: 200
+    end
+
+    
 end
