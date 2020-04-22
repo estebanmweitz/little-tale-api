fb52d666fa74a69ef0df97f340b4f3647e74e767
rafaeldault <rafaeldault@gmail.com>
2020-04-22 14:26:27 -0500

    fixes render json in User controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,18 +7,18 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         user = User.find(params[:id])
-        render json: users, status: 200
+        render json: user, status: 200
     end
 
     def create
         user = User.create(user_params)
-        render json: users, status: 200
+        render json: user, status: 200
     end
 
     def update
         user = User.find(params[:id])
         user.update(user_params)
-        render json: users, status: 200
+        render json: user, status: 200
     end
 
     def destroy
