a1a8647f83e3867651b80517298a19bfea732a7c
rafaeldault <rafaeldault@gmail.com>
2020-04-22 17:41:18 -0500

    adds validations to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
-    
 
+    validates: :text, :title, :user, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :posts
     has_many :comments
+
+    validates: :username, :email, presence: true
+    validates: :username, uniqueness: true
 end
