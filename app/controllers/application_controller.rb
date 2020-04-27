0a93a745b13d52e8eb822be5da0760c1455c508e
rafaeldault <rafaeldault@gmail.com>
2020-04-26 22:05:03 -0500

    new route for get_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::API
     include ::ActionController::Cookies
  
   def current_user
-    # User.first
     User.find_by(id: session[:user_id])
-    # ^^ will implement later
   end
 
   def logged_in?
