4d24eb3b661b6d473ea28c5e1c0761de459a5d7c
rafaeldault <rafaeldault@gmail.com>
2020-04-26 20:55:46 -0500

    rebuilt and remigrated db to add password digest

--- db/migrate/20200427014947_create_users.rb
+++ db/migrate/20200427014947_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :password_digest
       t.string :email
       t.boolean :admin, default: false
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_202530) do
+ActiveRecord::Schema.define(version: 2020_04_27_014947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_202530) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
